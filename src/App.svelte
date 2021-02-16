7490d337ed93d73de5b0ef8d5cc1886272cf8068
Tricia Saul <tricia@Tricias-Mac-Pro.local>
2021-02-16 17:06:15 -0500

    Init

---
+++ src/App.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import dummyData from './dummy.js';
+	import Filter from './components/Filter/Filter.svelte';
+
+	onMount(async() => {
+		await console.info(data)
+	})
+	// Response from REST API
+	export let data: any = JSON.parse(dummyData);
+</script>
+
+<main>
+	<Filter mode="market" {data} />
+</main>
+
+<style type="scss">
+	$color: blue;
+	main {
+		font-family: 'Roboto';
+		font-size: 18px;
+	}
+
+	h1 {
+		color: $color;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ src/components/Filter/Filter.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+  const validFilters = ["market", "name", "style"] as const;
+  type FilterMode = typeof validFilters[number];
+  export let mode: FilterMode;
+  export let data;
+</script>
+
+<article class="filters">
+    <div class="filter-picker">
+        <label class="large-label" for="filter-select">View galleries...</label>
+        <select name="modes" id="filter-select">
+            {#each validFilters as filter }
+              <option value="{ filter }">... by { filter }</option>
+            {/each}
+        </select>
+    </div>
+</article>
+
+<style type="scss">
+  $gray: #EAEAEA;
+  .filters {
+    background-color: $gray;
+    padding: 1.5rem;
+  }
+
+  .large-label {
+    font-size: 2rem;
+    font-weight: 100;
+  }
+
+  #filter-select, .large-label {
+    display: inline-block;
+  }
+</style>
